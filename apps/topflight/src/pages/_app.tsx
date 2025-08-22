fe78bd99349f70f7f2d75160686ed156bd748ce2
Sanay Yogesh Shah <sanayyog@amazon.com>
2025-08-22 15:52:40 -0700

    Initial commit

---
+++ apps/topflight/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app';
+import React, { FC } from 'react';
+import '../styles/globals.css';
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <Component {...pageProps} />
+  );
+};
+
+export default MyApp;
